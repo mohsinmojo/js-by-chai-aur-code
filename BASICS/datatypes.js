f734fc1987daee80b8a293f77173879137793455
mohsinmojo <141500795+mohsinmojo@users.noreply.github.com>
2025-01-08 06:54:38 +0000

    datatypes

---
+++ BASICS/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) we are using nodejs not browser
+
+console.log(3+
+    3)//code redability should be high
+
+    let name = "mohsin"
+    let age = 2
+    let isloggedIn = "false"
+    let state; 
+
+   //datatypes
+
+//nimber=> 2 to the power 52
+//bigint
+//string = ""
+//boolean => true/false
+//null => standalone value
+//undefinde =>
+//symbol => uniqueness
+//object 
+
+console.log(typeof undefined);//output of it undefined
+console.log(typeof null);//output of it object
