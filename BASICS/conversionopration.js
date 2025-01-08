d6063ffe46e764d3eecb36e3e4f3fdea445c4e8b
mohsinmojo <141500795+mohsinmojo@users.noreply.github.com>
2025-01-08 07:52:44 +0000

    conversion

---
+++ BASICS/conversionopration.js
@@ -0,0 +1,23 @@
+let score = false
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//"33" => 33
+//33abc => NaN
+//true =>1/false =>0
+
+let isLoggedIn = "mohsin"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn)
+//1 => true;0 => false
+//"" => false
+//"mohsin" => true
+
+let anyNmber = 1
+let stringNumber = String(anyNmber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
