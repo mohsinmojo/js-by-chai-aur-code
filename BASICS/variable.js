90732d63ca84a897303146c708edcfd258aade27
mohsinmojo <141500795+mohsinmojo@users.noreply.github.com>
2025-01-09 08:17:32 +0000

    variables

--- BASICS/variable.js
+++ BASICS/variable.js
@@ -4,9 +4,12 @@ var accountPassword ="843939"
 accountCity = "jaipur" 
 
 // accountId = "2"//NOT ALLOWD
-accountEmail ="mohd123@gmail.com"
-accountPassword = "345676"
-accountCity = "delhi"
-console.log(accountEmail);
-console.table([accountEmail, accountPassword, accountCity])
+// accountEmail ="mohd123@gmail.com"
+// accountPassword = "345676"
+// accountCity = "delhi"
+// console.log(accountEmail);
+// console.table([accountEmail, accountPassword, accountCity])
 // PREFER NOT TO USE VAR BECAUSE OF ISSUE IN BLOCK SCOPE AND FUNCTIONAL SCOPE
+
+
+
