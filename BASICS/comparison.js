e49dbbdb30a422ecbc648755bae7df01984c29f0
mohsinmojo <141500795+mohsinmojo@users.noreply.github.com>
2025-01-10 16:04:57 +0000

    comparison

---
+++ BASICS/comparison.js
@@ -0,0 +1,31 @@
+// console.log(2 > 1)
+// console.log(33 < 45);
+// console.log(2 >= 1);
+// console.log(55 == 55);
+// console.log(5 != 3);
+
+
+// console.log("2" > 1);
+// console.log("2" > "1");
+// console.log("02" > 1);
+// console.log("2" > 1);   we will avoid this type of comparison
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// ===(strict comparison)
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
