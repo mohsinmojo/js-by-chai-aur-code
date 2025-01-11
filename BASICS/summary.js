6c9dc3167184ae1e7ca6d5779e0d561f764ef2ee
mohsinmojo <141500795+mohsinmojo@users.noreply.github.com>
2025-01-11 12:44:28 +0000

    datatypesummary

---
+++ BASICS/summary.js
@@ -0,0 +1,30 @@
+// primitive datatypes
+
+// 7types String,Boolean,undefine,null,symbol,Bigint,Number
+ const value = 200
+const isLoggedIn = false
+ const score = null
+ let userEmail;
+let Name = "mohsin"
+const id = Symbol('123')
+ const anotherId = symbol('123')
+ const bigNumber =37498702n
+
+
+
+
+
+// Reference(Non primitive)
+// Array,objects,Functions
+const heros = ["iron man", "shaktiman", "rohit"]
+ let myAge = {
+    Name:"mohsin",
+    age:23,
+}
+
+const myFunction = function(){
+    console.log("hello mohsin");
+    
+}
+
+console.log( typeof myFunction);
