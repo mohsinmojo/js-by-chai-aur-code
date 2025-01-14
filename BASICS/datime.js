f1564459f9cb6260d75a8ec2bad6f5a388a8a752
mohsinmojo <141500795+mohsinmojo@users.noreply.github.com>
2025-01-14 09:28:04 +0000

    date

--- BASICS/datime.js
+++ BASICS/datime.js
@@ -8,8 +8,40 @@ let date = new Date()
 // console.log(date.toLocaleTimeString());
 // console.log(date.toTimeString());
 
-let newDate = new Date(2025, 0, 14)
-console.log(newDate.toString());
+// let newDate = new Date(2025, 0, 14)
+// console.log(newDate.toDateString());
+
+// let newDate = new Date(2025, 0, 14, 2, 26)
+// console.log(newDate.toLocaleString());
+
+// let newDate = new Date("2025-01-14")
+// console.log(newDate.toLocaleString());
+
+let newDate = new Date("01-14-2025")
+// console.log(newDate.toLocaleString());
+
+// ==================time======================================
+
+let timestamp = Date.now()
+// console.log(timestamp);
+// console.log(newDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+let oneDate = new Date()
+console.log(oneDate.getMonth() + 1);
+console.log(oneDate.getDay());
+
+oneDate.toLocaleString('default'{
+    weekday: "narrow"
+})
+
+
+
+
+
+
+
 
 
 ;
