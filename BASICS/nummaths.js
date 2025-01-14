fec2fc1719093e4be4edaed867f1b046a7d1d7b6
mohsinmojo <141500795+mohsinmojo@users.noreply.github.com>
2025-01-14 06:54:23 +0000

    number

---
+++ BASICS/nummaths.js
@@ -0,0 +1,52 @@
+const value = 5000
+// console.log(value);
+const score = new Number(1000.87804)
+// console.log(score.toString().charAt(1));
+// console.log(score.toFixed(2));
+
+const anyNumber = 23.8980
+// console.log(anyNumber.toPrecision(4));
+
+const oneNumber = 10000000
+// console.log(oneNumber.toLocaleString());
+// console.log(oneNumber.toLocaleString(`en-IN`));
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.EPSILON);
+
+// ======================Maths=======================================================
+
+
+// console.log(Math);
+// console.log(Math.abs(-19));//isse hum - ki value ko + main change kar sakte hai
+// console.log(Math.round(5.9));//isse hum value ko round up main le sakte hai
+// console.log(Math.ceil(5.9));//isse hum round up main upper value ke liye use karte hai
+// console.log(Math.floor(5.9));//isse hum round up main niche ki value ke liye use karte hai
+// console.log(Math.sqrt(225));//isse hum sqrt nikal sakte hai
+// console.log(Math.min(123, 234, 432));//minimum value
+// console.log(Math.max(123, 234, 432));//max value
+// console.log(Math.sin(45));//trigo value
+
+console.log(Math.random());//iski value hamesha 0-1 ke bich main aati hai
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 20 
+const max = 30 
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
