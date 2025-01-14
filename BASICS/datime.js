b486e1380ee083f12ba96ee7656cbd73af27b438
mohsinmojo <141500795+mohsinmojo@users.noreply.github.com>
2025-01-14 07:42:21 +0000

    datime

---
+++ BASICS/datime.js
@@ -0,0 +1,18 @@
+let date = new Date()
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toJSON())
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.toTimeString());
+
+let newDate = new Date(2025, 0, 14)
+console.log(newDate.toString());
+
+
+;
+
+
+
