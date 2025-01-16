ed7c3098ae1ab34a18eae6308c5a2f1e1beddb5b
mohsinmojo <141500795+mohsinmojo@users.noreply.github.com>
2025-01-16 09:26:41 +0000

    objects

---
+++ basics1/objects.js
@@ -0,0 +1,40 @@
+//singleton
+// object.create
+
+
+// object literals
+
+const mySymbol = Symbol("mykey1")
+
+
+const myUser = {
+    name: "mohsin",
+    [mySymbol]: "mykey1",
+    age: 40,
+    email: "mohsdhd@gmail.com",
+    Id: "rajamohan",
+    password: 67398,
+    logIn: false,
+    "full name": "mohammad moshin",
+}
+// console.log(myUser.name);
+// console.log(myUser["full name"]);
+// console.log(myUser[mySymbol]);
+
+myUser.email = "mohdmohsin@gmail.com"
+// console.log(myUser.email);
+// Object.freeze(myUser)
+myUser.email = "mohdanish@gmail.com"
+// console.log(myUser);
+myUser.greeting = function(){
+    console.log("hello user");
+    
+}
+
+myUser.greetingtwo = function(){
+    console.log(`hello user ${this.name} how are you`);
+    
+}
+console.log(myUser.greeting());
+console.log(myUser.greetingtwo());
+
