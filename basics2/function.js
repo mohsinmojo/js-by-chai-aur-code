ddb680651dd22c1f208f827525b8948e2f67d423
mohsinmojo <141500795+mohsinmojo@users.noreply.github.com>
2025-01-18 15:37:34 +0000

    function

---
+++ basics2/function.js
@@ -0,0 +1,18 @@
+function myRealname(){
+    console.log("m");
+    console.log("o");
+    console.log("h");
+    console.log("s");
+    console.log("i");
+    console.log("n");
+    
+}
+
+// myRealname() 
+
+function addTwoNumber(number1, number2){
+
+    console.log(number1 + number2);
+    
+}
+addTwoNumber(9, 8)
